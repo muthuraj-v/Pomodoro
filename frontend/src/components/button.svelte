3ca3ca164b030954ce6dda31911c571ce272062d
muthuraj-v <muthurajkpr8@gmail.com>
2025-07-16 20:21:15 +0530

    added resume fn

--- frontend/src/components/button.svelte
+++ frontend/src/components/button.svelte
@@ -1,16 +1,57 @@
 <script lang="ts">
+  import {
+    buttonClick,
+    type buttonType,
+    activeTab,
+    pomodoro,
+    shortBreak,
+    longBreak,
+  } from "./store";
   let isTrue = false;
+  const buttons: buttonType[] = ["Start", "Pause", "Resume"];
+  buttonClick.set("Start");
   function onClick() {
     isTrue = !isTrue;
+    if ($buttonClick === "Start") {
+      buttonClick.set(buttons[1]);
+    } else if ($buttonClick === "Pause") {
+      buttonClick.set(buttons[2]);
+    } else if ($buttonClick === "Resume") {
+      buttonClick.set(buttons[1]);
+    } else {
+      buttonClick.set(buttons[1]);
+    }
+  }
+  function reset() {
+    if ($activeTab === "Pomodoro") {
+      buttonClick.set("Start");
+
+      pomodoro.set(25);
+    } else if ($activeTab === "Short Break") {
+      buttonClick.set("Start");
+
+      shortBreak.set(5);
+    } else if ($activeTab === "Long Break") {
+      buttonClick.set("Start");
+
+      longBreak.set(15);
+    }
+    buttonClick.set("Start");
   }
 </script>
 
 <main>
-  <button type="button" on:click={onClick} class:paused={isTrue}>
-    {isTrue ? "Pause" : "Start"}
+  <button
+    type="button"
+    on:click={onClick}
+    class:paused={$buttonClick === "Pause"}
+    class:resume={$buttonClick === "Resume"}
+  >
+    <!-- {isTrue ? "Pause" : "Start"} -->
+    {$buttonClick}
   </button>
-  {#if isTrue}
-    <button type="button" id="reset">&#8634;</button>
+  {#if $buttonClick != "Start"}
+    <button type="button" id="reset" on:click={reset}>&#8634;</button>
   {/if}
 </main>
 
@@ -40,19 +81,6 @@
   main button:hover {
     background-color: #50506e;
   }
-  main button.paused {
-    background-color: #ccc;
-    color: black;
-    border-bottom: none;
-  }
-  main button.paused:active {
-    background-color: rgba(204, 204, 204, 0.7);
-    color: black;
-  }
-  main button:active {
-    background-color: rgb(40, 40, 60);
-    transform: scale(0.97);
-  }
 
   #reset {
     border: none;
@@ -73,5 +101,31 @@
 
   #reset:hover {
     background-color: #666;
+  }
+  main button.paused {
+    background-color: #ccc;
+    color: black;
+    border-bottom: none;
+  }
+
+  main button.paused:active {
+    background-color: rgba(204, 204, 204, 0.7);
+    color: black;
+  }
+
+  main button.resume {
+    background-color: #cfe7cf;
+    color: #1e1e1e;
+    border-bottom: none;
+  }
+
+  main button.resume:active {
+    background-color: rgba(207, 231, 207, 0.7);
+    color: #1e1e1e;
+  }
+
+  main button:active {
+    background-color: rgb(40, 40, 60);
+    transform: scale(0.97);
   }
 </style>
--- frontend/src/components/store.ts
+++ frontend/src/components/store.ts
@@ -1,6 +1,7 @@
 import { writable } from "svelte/store";
 
 export type TabType = "Pomodoro" | "Short Break" | "Long Break";
+export type buttonType = "Start" | "Pause" | "Resume";
 export type PopupType = "Settings" | "About";
 export const activeTab = writable<TabType>("Pomodoro");
 export const pomodoro = writable<number>(25);
@@ -8,3 +9,4 @@ export const shortBreak = writable<number>(5);
 export const longBreak = writable<number>(15);
 export const isPopup = writable<boolean>(false);
 export const PopupTap = writable<PopupType>("Settings");
+export const buttonClick = writable<buttonType>("Start");
